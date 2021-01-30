45e711b5554a54e9482b2aed69e2dbe57b43f2e9
ankushKun <ankus4singh@gmail.com>
2021-01-30 15:28:17 +0530

    updated readme

---
+++ Animedoro/doro/doro.js
@@ -0,0 +1,123 @@
+var interval;
+let endTime = Date.now() + (40 * 60 * 1000);
+let watchingAnime = false;
+let animeTime = 20;
+let workTime = 40;
+let timeMins = workTime;
+
+const minsCounter = document.querySelector('.minutes');
+const secsCounter = document.querySelector('.seconds');
+const startButton = document.querySelector('#start-button');
+const plus5 = document.querySelector('#plus5');
+const nextEventInfo = document.querySelector('.next-event-info')
+
+const beep = document.querySelector("#notificationSound");
+
+startButton.textContent = "START";
+plus5.textContent = "+5";
+plus5.style.visibility = 'hidden';
+
+
+const startCountdown = function (mins, secs) {
+    const now = Date.now();
+    endTime = now + (mins * 60 * 1000) + (secs * 1000);
+    displayTime(Math.round((endTime - Date.now()) / 1000));
+    plus5.style.visibility = 'visible';
+    interval = setInterval(() => {
+        const timeLeft = Math.round((endTime - Date.now()) / 1000);
+        if (timeLeft < 0) {
+            beep.play();
+            if (watchingAnime) {
+                // working time starts
+                clearInterval(interval);
+                nextEventInfo.textContent = "Dont you dare click on next Episode!"
+                startButton.textContent = "START";
+                plus5.style.visibility = 'hidden';
+                timeMins = workTime;
+                watchingAnime = false;
+                notify("Anime time has ended", "Get back to work\nAND DONT YOU DARE CLICK ON NEXT EPISODE!");
+                return;
+            }
+            else {
+                // anime time starts
+                clearInterval(interval);
+                timeMins = animeTime;
+                nextEventInfo.textContent = "You can watch Anime now"
+                plus5.style.visibility = 'hidden';
+                startButton.textContent = "START";
+                watchingAnime = true;
+                notify("You can take a break now", "Go watch that episode!\n");
+                return;
+            }
+        }
+        displayTime(timeLeft);
+    }, 100);
+}
+
+const displayTime = function (seconds) {
+    let minutesRemaining = Math.floor(seconds / 60);
+    let secondsRemaining = seconds % 60;
+    minsCounter.textContent = (minutesRemaining < 10 ? '0' : '') + minutesRemaining;
+    secsCounter.textContent = (secondsRemaining < 10 ? '0' : '') + secondsRemaining;
+    console.log(minutesRemaining, secondsRemaining);
+}
+
+const add5mins = function () {
+    endTime += (5 * 60 * 1000);
+    //displayTime(Date.now() - endTime);
+    updateEventDetails(endTime);
+}
+
+const updateEventDetails = function (timestamp) {
+    let dateObj = new Date(timestamp);
+    let hours = dateObj.getHours();
+    hours = (hours < 10 ? '0' : '') + hours;
+    let mins = dateObj.getMinutes();
+    mins = (mins < 10 ? '0' : '') + mins;
+    if (watchingAnime) {
+        nextEventInfo.textContent = 'Stop Anime at ' + hours + ':' + mins;
+    }
+    else {
+        nextEventInfo.textContent = 'Next Episode at ' + hours + ':' + mins;
+    }
+
+}
+
+function startClicked() {
+    checkPerms();
+    startCountdown(timeMins, 0);
+    updateEventDetails(endTime);
+    startButton.textContent = "RESTART";
+}
+
+startButton.addEventListener('click', startClicked);
+plus5.addEventListener('click', add5mins);
+
+//startCountdown(1, 5);
+//add5mins();
+//updateEventDetails(endTime)
+
+/*NOTIFICATIONS*/
+
+const checkPerms = function () {
+    if (Notification.permission === "granted") {
+        //alert("we have permission");
+        console.log("notifications allowed");
+    } else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(permission => {
+            console.log(permission);
+        });
+    }
+}
+
+const notify = function (heading, desc) {
+    checkPerms();
+
+    const notification = new Notification(heading, {
+        body: desc,
+        icon: "../Images/icon.png"
+    });
+    beep.play();
+}
+
+checkPerms();
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Animedoro Timer
-An Animedoro timer for all the weebs out there | productivity : stonks
+# Animedoro Timer ⏱
+<div align="center">
+    <a href="https://ankushKun.github.io/projects/animedoro-timer/Animedoro.html">
+        <img src="./Animedoro/Images/icon.png" width="30%">
+    </a>
+</div>
+
+## What is AnimeDoro?
+
+It's like **pomodoro**, but with **Anime**
+Instead of regular 25+5 pomodoro technique,
+Animedoro uses 40+20 timers
+you work at your own pace for 40 to 60 minutes
+then watch an episode of anime for 20 minutes
+and continue the cycle.\
+***For more info watch this video by [Josh Chen](https://www.youtube.com/watch?v=bUjGZJIgse0)***\
+productivity : 📈
+
+## Use the **[animedoro timer](https://ankushKun.github.io/projects/animedoro-timer/Animedoro.html)** 😄
+
+## Screenshots
+
+![homepage](./Animedoro/Images/homepage.png)
+![timer](./Animedoro/Images/timer.png)
+![notification](./Animedoro/Images/notif.PNG)
+
+## TODO
+
+- [ ] A settings section to set custom timers
+- [ ] A browser extension?
