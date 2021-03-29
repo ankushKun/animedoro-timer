cf8674ee56408c00769fd75cd17661bbc6454f54
ankushKun <ankus4singh@gmail.com>
2021-03-29 19:37:48 +0530

    added pause and session counters

--- Animedoro/doro/doro.js
+++ Animedoro/doro/doro.js
@@ -1,29 +1,65 @@
 var interval;
 let endTime = Date.now() + (40 * 60 * 1000);
 let watchingAnime = false;
-let animeTime = 20;
-let workTime = 40;
+let animeTime = 1;
+let workTime = 2;
 let timeMins = workTime;
+let paused = false;
 
 const minsCounter = document.querySelector('.minutes');
 const secsCounter = document.querySelector('.seconds');
 const startButton = document.querySelector('#start-button');
 const plus5 = document.querySelector('#plus5');
+const pause = document.querySelector('#pause');
+const play = document.querySelector('#play');
 const nextEventInfo = document.querySelector('.next-event-info')
+const animeSessionCounter = document.querySelector('.anime-session-counter')
+const studySessionCounter = document.querySelector('.study-session-counter')
 
 const beep = document.querySelector("#notificationSound");
 
 startButton.textContent = "START";
 plus5.textContent = "+5";
 plus5.style.visibility = 'hidden';
+pause.style.visibility = 'hidden';
+play.style.visibility = 'hidden';
 
+// get values and initialize if non existent
+let animeSessions = localStorage.getItem("animeSessions") ? localStorage.getItem("animeSessions") : 0;
+let studySessions = localStorage.getItem("studySessions") ? localStorage.getItem("studySessions") : 0;
+localStorage.setItem("animeSessions", animeSessions);
+localStorage.setItem("studySessions", studySessions);
+
+function updateSessionCounters() {
+    animeSessions = parseInt(localStorage.getItem("animeSessions"));
+    animeSessionCounter.textContent = animeSessions + " Anime Sessions";
+    studySessions = parseInt(localStorage.getItem("studySessions"));
+    studySessionCounter.textContent = studySessions + " Study Sessions";
+}
+updateSessionCounters();
+
+function addAnimeSession() {
+    localStorage.setItem("animeSessions", parseInt(animeSessions) + 1);
+    updateSessionCounters();
+}
+function addStudySession() {
+    localStorage.setItem("studySessions", parseInt(studySessions) + 1);
+    updateSessionCounters();
+}
 
 const startCountdown = function (mins, secs) {
     const now = Date.now();
     endTime = now + (mins * 60 * 1000) + (secs * 1000);
     displayTime(Math.round((endTime - Date.now()) / 1000));
     plus5.style.visibility = 'visible';
+    pause.style.visibility = 'visible';
+    play.style.visibility = 'hidden';
+    paused = false;
     interval = setInterval(() => {
+        if (paused) {
+            endTime += (100); // keeps adding 100ms because interval is called every 100ms
+            return;
+        }
         const timeLeft = Math.round((endTime - Date.now()) / 1000);
         if (timeLeft < 0) {
             beep.play();
@@ -36,6 +72,7 @@ const startCountdown = function (mins, secs) {
                 timeMins = workTime;
                 watchingAnime = false;
                 notify("Anime time has ended", "Get back to work\nAND DONT YOU DARE CLICK ON NEXT EPISODE!");
+                addAnimeSession();
                 return;
             }
             else {
@@ -47,11 +84,12 @@ const startCountdown = function (mins, secs) {
                 startButton.textContent = "START";
                 watchingAnime = true;
                 notify("You can take a break now", "Go watch that episode!\n");
+                addStudySession();
                 return;
             }
         }
         displayTime(timeLeft);
-    }, 100);
+    }, 100);// 100ms interval
 }
 
 const displayTime = function (seconds) {
@@ -59,7 +97,7 @@ const displayTime = function (seconds) {
     let secondsRemaining = seconds % 60;
     minsCounter.textContent = (minutesRemaining < 10 ? '0' : '') + minutesRemaining;
     secsCounter.textContent = (secondsRemaining < 10 ? '0' : '') + secondsRemaining;
-    console.log(minutesRemaining, secondsRemaining);
+    //console.log(minutesRemaining, secondsRemaining);
 }
 
 const add5mins = function () {
@@ -94,8 +132,17 @@ function startClicked() {
     startButton.textContent = "RESTART";
 }
 
-startButton.addEventListener('click', startClicked);
-plus5.addEventListener('click', add5mins);
+function pauseCountdown() {
+    paused = true;
+    pause.style.visibility = 'hidden';
+    play.style.visibility = 'visible';
+}
+
+function resumeCountdown() {
+    paused = false;
+    pause.style.visibility = 'visible';
+    play.style.visibility = 'hidden';
+}
 
 //startCountdown(1, 5);
 //add5mins();
